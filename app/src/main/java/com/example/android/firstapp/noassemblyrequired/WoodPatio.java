ccfec0d4ae0168269fe6e17c78bf793a6bd04403
dleon <dleon@students.mchenry.edu>
2019-09-02 07:04:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/firstapp/noassemblyrequired/WoodPatio.java
@@ -0,0 +1,14 @@
+package com.example.android.firstapp.noassemblyrequired;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WoodPatio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wood_patio);
+    }
+}
